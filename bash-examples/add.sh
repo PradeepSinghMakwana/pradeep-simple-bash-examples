47aa3ffa6ea777852ac684a18f8d0642ba4fcb93
ashishpathak760 <pradeepsinghmakwana@gmail.com>
2022-04-04 18:48:16 +0530

    separated functions from calculator to individual callable scripts

---
+++ bash-examples/add.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Author: Pradeep Singh Makwana (pradeepsighmakwana@gmail.com)
+echo "$1+$2"|bc
---
+++ bash-examples/divide.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Pradeep Singh Makwana (pradeepsighmakwana@gmail.com)
+first=$1
+second=$2
+echo "scale=12;$first/$second"|bc
